749efff957e8abec37a63af4a9e3aedf22333e80
gregorscholz <gregorscholz@proton.me>
2022-07-26 21:37:32 +0200

    added almost all the drawings also small changes

--- SAPC/src/shop.py
+++ SAPC/src/shop.py
@@ -1,7 +1,7 @@
 class Shop():
 
     def __init__(self):
-        self.offer = [None, None, None, None, None]
+        self.board = [None, None, None, None, None]
         self.probs = [0.8, 0.2, 0.0, 0.0, 0.0]
 
     def generate_pshop(self, round: int):
